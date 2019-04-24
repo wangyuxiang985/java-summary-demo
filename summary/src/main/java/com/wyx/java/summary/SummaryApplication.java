290e5bc66e67965cd8639e1ae57f6806081497b9
wangyuxiang985 <wangyuxiang985@gmail.com>
2019-04-24 23:51:51 +0800

    first

---
+++ summary/src/main/java/com/wyx/java/summary/SummaryApplication.java
@@ -0,0 +1,13 @@
+package com.wyx.java.summary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SummaryApplication.class, args);
+	}
+
+}
