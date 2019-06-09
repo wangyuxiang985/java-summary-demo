d3a10e4756e9070d45a724b5a257842b64c44961
wangyuxiang985 <wangyuxiang985@gmail.com>
2019-06-09 16:34:43 +0800

    线程池相关demo

--- summary/src/main/java/com/wyx/java/summary/threadpool/MyThreadPoolDemo.java
+++ summary/src/main/java/com/wyx/java/summary/threadpool/MyThreadPoolDemo.java
@@ -1,8 +1,6 @@
 package com.wyx.java.summary.threadpool;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 /**
  * @author: yuxiang
@@ -13,12 +11,30 @@ public class MyThreadPoolDemo {
     public static void main(String[] args) {
         //查看电脑cpu数量
         //System.out.println(Runtime.getRuntime().availableProcessors());
+        ExecutorService executorService = new ThreadPoolExecutor(2, 5, 1L, TimeUnit.SECONDS, new LinkedBlockingDeque<>(3), Executors.defaultThreadFactory(), new ThreadPoolExecutor.DiscardPolicy());
+
+        try{
+            for (int i = 1; i <= 10; i++) {//模拟十个请求
+                executorService.execute(() -> {
+                    System.out.println(Thread.currentThread().getName() + "\t 线程执行");
+                });
+                //TimeUnit.MILLISECONDS.sleep(2000);
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }finally{
+            executorService.shutdown();
+        }
+
+    }
+
+    private static void threadPoolInitByExecutors() {
         //一池五个线程
-        //ExecutorService executorService =
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
         //一池一线程
         //ExecutorService executorService = Executors.newSingleThreadExecutor();
         //一池N线程
-        ExecutorService executorService = Executors.newCachedThreadPool();
+        //ExecutorService executorService = Executors.newCachedThreadPool();
 
         try{
             for (int i = 1; i <= 10; i++) {//模拟十个请求
